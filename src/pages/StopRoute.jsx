e8e9b183048c361dc4e16e675656e07ace9dd2ae
Daniel <danielh839@gmail.com>
2020-09-09 18:05:15 -0400

    Full schedule button

--- src/pages/StopRoute.jsx
+++ src/pages/StopRoute.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { Page, IndentedParagraph, Section, ErrorPage, ErrorMessage, StopTimeList, BusList, LiveBusMap, LoadingSpinner } from "components";
-import { stopRouteQuery } from "api"
+import { stopRouteQuery, stopRouteScheduleQuery } from "api"
 
+const FullSchedule = ({ id }) => {
+    let { data, error, loading } = useQuery(stopRouteScheduleQuery(id));
+    if (data) {
+        return (
+            <Section title={"Full Schedule"}>
+                <IndentedParagraph style={{wordBreak: 'break-all' }}><StopTimeList stopTimes={data.stopRoute.schedule.all}/></IndentedParagraph>
+            </Section> 
+        )
+    } else {
+        return null
+    }
+}
+
+const FullScheduleWrapper = ({ id }) => {
+    let [show, setShow] = useState(false);
+
+    return (
+        <div>
+            {show ? (
+                <FullSchedule id={id}/>
+            ) : (
+                <button className="btn btn-primary font-weight-bold" onClick={() => setShow(true)}>Full Schedule</button>
+            ) }
+        </div>
+    )
+}
 const StopRoute = () => {
     let { id } = useParams();
     let { data, error, loading } = useQuery(stopRouteQuery(id));
@@ -25,6 +51,7 @@ const StopRoute = () => {
                 <IndentedParagraph>
                     <StopTimeList stopTimes={stopRoute.schedule.next} number={stopRoute.number} headsign={stopRoute.headsign} />
                 </IndentedParagraph>
+                <FullScheduleWrapper id={id}/>
             </Section>
 
             {data.stopRoute.liveBusData.busCount === 0 && (
